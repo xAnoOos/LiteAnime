e3da5f5ca0062a1426abb9c19cde2e032fc94c90
Anas Obaid <Anas_Obaid>
2025-04-02 18:16:16 +0300

    save changes

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-
 from .models import News, NewsComment
 
-admin.site.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'created_at']
+    exclude = ['user']  
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(News, NewsAdmin)
 admin.site.register(NewsComment)
--- news/models.py
+++ news/models.py
@@ -6,7 +6,7 @@ class News(models.Model):
     content = models.TextField()
     image = models.ImageField(upload_to='news_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
